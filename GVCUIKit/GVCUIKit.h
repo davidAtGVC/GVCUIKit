9698f1143bb63a2815f98f3396acedf7c32cc672
David Aspinall <daspinall@mac.com>
2012-02-24 11:47:35 -0500

    Initial Commit

---
+++ GVCUIKit/GVCUIKit.h
@@ -0,0 +1,13 @@
+//
+//  GVCUIKit.h
+//  GVCUIKit
+//
+//  Created by David Aspinall on 12-02-24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GVCUIKit : NSObject
+
+@end
---
+++ GVCUIKit/GVCUIKit.m
@@ -0,0 +1,23 @@
+//
+//  GVCUIKit.m
+//  GVCUIKit
+//
+//  Created by David Aspinall on 12-02-24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GVCUIKit.h"
+
+@implementation GVCUIKit
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
