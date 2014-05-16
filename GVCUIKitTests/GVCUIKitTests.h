a9c8237358f33b71e9d6f364d74d2ad91d53b480
David Aspinall <daspinall@global-village.net>
2014-05-16 15:02:06 -0600

    Updated to XCTest

--- GVCUIKitTests/GVCUIKitTests.h
+++ GVCUIKitTests/GVCUIKitTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2012 Global Village Consulting. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface GVCUIKitTests : SenTestCase
+@interface GVCUIKitTests : XCTestCase
 
 @end
